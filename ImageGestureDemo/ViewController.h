6d9e5bd928f2912c2d04eabc6e0c464c79a21a10
ByteSlinger <michael@byteslinger.net>
2018-06-21 15:00:41 -0700

    Initial Commit

---
+++ ImageGestureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageGestureDemo
+//
+//  Created by Michael S. Hearn on 6/21/18.
+//  Copyright © 2018 ByteSlinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
